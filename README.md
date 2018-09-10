9153ef68ca450855fb3149121d1db32336571d5a
Jhonatan <jhonatan@offertech.com.br>
2018-09-10 16:29:30 -0300

    second attempt

--- README.md
+++ README.md
@@ -7,7 +7,17 @@ As my knowledge on the topic, R and Machine Learning, is yet supperficial, I've 
 
 # Kaggle Titanic Dataset
 
-#### First attempt
+#### First attempt -- They all died
 The most basic guess you could make is that they all died, and that was my first attempt, and guess what, it scored 62.679%.
 
 ![alt text](https://raw.githubusercontent.com/silvajhonatan/titanic/master/img/first_attempt.png)
+
+#### Second attempt -- Gender model
+Now we can start to analyse more the data, something you could try to see the correlation is between the genders, we
+have in our database the $Sex column, with this we can analyse and see that 74.20% of the women survived against just
+18.89% of the men. So now we can update our last guess with this new information, we can set to survived
+all the women in our test dataset. This is our new score!  
+![alt text](https://raw.githubusercontent.com/silvajhonatan/titanic/master/img/gender_attempt.png)
+After that we could try to see some correlation between the survived column and the age, we can
+create a new column if the person was under 18 or not. But as we can see this seems to not 
+matter in this case (see the code).
--- titanic_r_analysis.R
+++ titanic_r_analysis.R
@@ -2,9 +2,38 @@ setwd("~/aside_projects/datascience/titanic")
 train <- read.csv("~/aside_projects/datascience/titanic/train.csv")
 test <- read.csv("~/aside_projects/datascience/titanic/test.csv")
 
+## First attempt
 str(train)
 # Inicial guess, they all died MUAHAHA!
 test$Survived <- rep(0,418)
 submit <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
-write.csv(submit, file = "./they_all_died.csv", row.names = FALSE)
-test
+write.csv(submit, file = "./submissions/they_all_died.csv", row.names = FALSE)
+
+## Second attempt
+
+# Let's analyse if we can make any correlation
+# between sex/age and if they survived
+summary(train$Sex)
+# The final parameter is for prop.table to analyse
+# specific by the Sex and not to divide by all
+# values
+prop.table(table(train$Sex,train$Survived),1)
+# We see that most females survived when most man didn't 
+# using this data we can change or prediction to take this
+# information in consideration
+are_female <- test$Sex == "female"
+test$Survived[are_female] <- 1
+submit <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
+write.csv(submit, file = "./submissions/gender_model.csv", row.names = FALSE)
+
+# Now we can also understand if the age couldn't be also
+# related 
+summary(train$Age)
+# We see that we have some NA's, we could subs this NA's to the mean value
+# But we'll create another column indicating if the person is a child. 
+train$Child <- 0
+is_under_age <- train$Age < 18
+train$Child[is_under_age] <- 1
+aggregate(Survived ~ Child + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
+# We see that independent if you're a child or an adult 
+# it didn't matter
